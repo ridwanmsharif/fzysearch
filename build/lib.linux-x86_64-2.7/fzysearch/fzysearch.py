b774e105f878793dd66f34f26ccde9a45310b06c
Ridwan Sharif <you@example.com>
2016-06-27 19:48:30 +0400

    First Commit

---
+++ build/lib.linux-x86_64-2.7/fzysearch/fzysearch.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+list_input = []
+search_input = ""
+
+
+def fuzzymatch(input, gallery):
+
+	dictionary = {}
+
+	for column in gallery:
+
+		search = input.lower()
+		library = column.lower()
+		search_index = 0
+		lib_index = 1
+
+		for letter in library:
+
+			if lib_index >= len(library) and search_index == len(search) - 1 and library[len(library)-1] == search[len(search) -1]:
+				
+				if dictionary.get(lib_index) != None :
+					dictionary[lib_index].append(library)
+				else:
+					dictionary[lib_index] = [library]
+				break
+
+			if search_index == len(search):
+				
+				if dictionary.get(lib_index) != None :
+					dictionary[lib_index].append(library)
+				else:
+					dictionary[lib_index] = [library]
+				break
+							
+			if lib_index >= len(library):
+				break
+
+			if letter == search[search_index]:
+				search_index += 1
+				lib_index += 1
+			else:
+				lib_index += 1 
+	
+	for key in sorted(dictionary):
+		print dictionary[key]
+	
+
+
+print datetime.now()
+fuzzymatch(search_input,list_input)
+print datetime.now()
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fzysearch',
+      version='0.1',
+      description='FuzzyMatches a string to a list and returns objects from the list in order of relevance',
+      url='http://github.com/ridwanmsharif/fuzzymatch',
+      author='Ridwan M. Sharif',
+      author_email='ridwanmsharif@hotmail.com',
+      license='MIT',
+      packages=['fzysearch'],
+      zip_safe=False)
