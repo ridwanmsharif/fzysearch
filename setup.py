2677408142f8ba5eed63276cdef36af74cb5dff9
Ridwan Sharif <ridwanmsharif@hotmail.com>
2016-07-22 00:08:09 +0400

    bumped up version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fzysearch',
-      version='0.3',
+      version='0.4',
       description='fuzzy searches a string input to a list and returns results from the list in order of relevance',
       url='http://github.com/ridwanmsharif/fzysearch',
       author='Ridwan M. Sharif',
